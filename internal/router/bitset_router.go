ab6999e7a94974a5edd415840341f417e6182286
Will Domine <willdomine1@outlook.com>
2026-02-05 10:43:14 -0600

    fixed the proto file and compiled + Benchmarks to regular heap allocation

--- internal/router/bitset_router.go
+++ internal/router/bitset_router.go
@@ -6,14 +6,20 @@ type Router struct {
 	requestPool sync.Pool
 }
 
+//Pool Object
 type BitContext struct {
 	Flags uint32
+}
+
+//Resets struct so it can be reused safely
+func (b *BitContext) Reset() {
+	b.Flags = 0
 }
 
 func NewRouter() *Router {
 	return &Router{
 		requestPool: sync.Pool{
-			New: func() any {return new(BitContext)},
+			New: func() any { return new(BitContext) },
 		},
 	}
 }
@@ -32,8 +38,8 @@ func (r *Router) Decide(flags uint32) (string, bool) {
 	// Bitwise for premium
 	const PremiumUserBit = 1 << 3 // 0x08
 
-	if flags & PremiumUserBit != 0 {
+	if flags&PremiumUserBit != 0 {
 		return "premium-backend", true
 	}
-	return "standard-backend", false
+	return "standard-backend", true
 }
--- main.go
+++ main.go
@@ -1,2 +1,55 @@
 package main
 
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/WillDomine/bitmesh-gateway/internal/handler"
+	"github.com/WillDomine/bitmesh-gateway/internal/router"
+	"github.com/WillDomine/bitmesh-gateway/api/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := ":42000"
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	//Initalizes the router in bitset_router.go and applies to grpcHandler 
+	coreRouter := router.NewRouter()
+	grpcHandler := &handler.GRPCHandler{
+		Router: coreRouter,
+	}
+
+	//The GRPC server setup
+	grpcServer := grpc.NewServer()
+	pb.RegisterGatewayRouterServer(grpcServer, grpcHandler)
+
+	//Debugging 
+	reflection.Register(grpcServer)
+
+	//Run server in Goroutine
+	go func() {
+		log.Printf("Bitmesh Gateway starting on %s", port)
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	//Graceful shutdown waiting for interrupt signal
+	stopChain := make(chan os.Signal, 1)
+	signal.Notify(stopChain, os.Interrupt, syscall.SIGTERM)
+
+	<-stopChain
+	log.Printf("Shutting down server")
+
+	grpcServer.GracefulStop()
+	log.Printf("Server stopped")
+}
